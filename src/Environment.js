2541e25dcfcc08bd2c801eae7112815131db06f9
Abdul Raseeth <abdulraseeth04@gmail.com>
2025-01-28 12:11:28 +0530

    environment file changed for deployment

--- src/Environment.js
+++ src/Environment.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://simple-crud-app-backend-kappa.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
