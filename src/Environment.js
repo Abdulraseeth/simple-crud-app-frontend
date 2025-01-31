ec7ccba06b205345735d53a166bab0e9585ca789
Abdul Raseeth <abdulraseeth04@gmail.com>
2025-01-31 10:41:44 +0530

    backend cors error fixed

--- src/Environment.js
+++ src/Environment.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://simple-crud-app-backend-cgepagq57-abdulraseeths-projects.vercel.app",
+    "https://simple-crud-app-backend-5wgscckg6-abdulraseeths-projects.vercel.app/",
   // baseURL: "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
