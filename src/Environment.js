c5f7a5ef141348afd19cee47ed29899b41537c10
Abdul Raseeth <abdulraseeth04@gmail.com>
2025-01-31 10:24:22 +0530

    backend url updated

--- src/Environment.js
+++ src/Environment.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "https://simple-crud-app-backend-cgepagq57-abdulraseeths-projects.vercel.app",
+    "https://simple-crud-app-backend-6f39z7o5u-abdulraseeths-projects.vercel.app",
   // baseURL: "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
